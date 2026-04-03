65ff9fd9e254043a99db79359e1d109d235193a4
Engy-Elyamany <engyelyamany26@gmail.com>
2026-04-03 23:27:00 +0200

    add  port initialization

--- GPIO_driver.c
+++ GPIO_driver.c
@@ -8,7 +8,7 @@ void GPIO_PinInit(GPIO_PinConfig *config)
       if (config->direction == GPIO_INPUT)
       {
             // write 1 in tris register at the corresponding pin location
-            SET_BIT(*tris,config->pin);
+            SET_BIT(*tris, config->pin);
             //*tris |= (1 << config->pin);
       }
       else
@@ -16,11 +16,13 @@ void GPIO_PinInit(GPIO_PinConfig *config)
             // write 0 in tris register at the corresponding pin location
             *tris &= ~(1 << config->pin);
             if (config->initial_state == GPIO_LOW)
-            {      CLR_BIT(*port, config->pin);
+            {
+                  CLR_BIT(*port, config->pin);
                   //*port &= ~(1 << config->pin);
             }
             else
-            {      SET_BIT(*port,config->pin);
+            {
+                  SET_BIT(*port, config->pin);
                   //*port |= (1 << config->pin);
             }
       }
@@ -30,23 +32,27 @@ void GPIO_WritePin(GPIO_PinConfig *config, uint8 value)
 {
       volatile unsigned char *port = PORT_REG[config->port];
       if (value == GPIO_HIGH)
-      {      SET_BIT(*port,config->pin);
+      {
+            SET_BIT(*port, config->pin);
             //*port |= (1 << config->pin);
       }
       else
-      {      CLR_BIT(*port, config->pin);
+      {
+            CLR_BIT(*port, config->pin);
             //*port &= ~(1 << config->pin);
       }
 }
 
 uint8 GPIO_ReadPin(GPIO_PinConfig *config)
 {
-      if(GET_BIT(*port,config->pin))
+      volatile unsigned char *port = PORT_REG[config->port];
+      if (GET_BIT(*port, config->pin))
             return GPIO_HIGH;
       return GPIO_LOW;
 }
 
 void GPIO_TogglePin(GPIO_PinConfig *config)
 {
-      TOG_BIT(*port,config->pin);
+      volatile unsigned char *port = PORT_REG[config->port];
+      TOG_BIT(*port, config->pin);
 }
